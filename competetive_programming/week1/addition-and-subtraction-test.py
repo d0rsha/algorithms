11f9c54210b8dd6abd3a7a9fc77605ff1da8e31a
Anders Vrethem <dersi_91@hotmil.com>
2020-01-20 22:40:07 +0100

    Alittle bit neater yes

--- competetive_programming/week1/addition-and-subtraction-test.py
+++ competetive_programming/week1/addition-and-subtraction-test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from unittest import TestCase
 import subprocess
+import random
 TESTNBR = 0
 
 
@@ -14,10 +15,11 @@ def execute(argument):
     proc = subprocess.Popen(argument, shell=True, stdin=subprocess.PIPE,
                             stdout=subprocess.PIPE, stderr=subprocess.STDOUT, close_fds=True)
     proc.wait()
-    return proc
+    return get_output(proc)
 
 
 def get_output(proc):
+    """Get trimmed process output"""
     output = ''
     for line in iter(proc.stdout.readline, ''):
         if not line:
@@ -27,81 +29,55 @@ def get_output(proc):
 
 
 def testUnit(x, y, z):
+    """
+    Test with given parameters
+    # ToDo: Make multiprocess task?
+    """
+
     global TESTNBR
     TESTNBR += 1
-    d = {'x': x, 'y': y, 'z': z}
+    dictionary = {'x': x, 'y': y, 'z': z}
+    print(f"Test{TESTNBR}: {dictionary}")
 
     execute(f'echo {x} {y} {z} > sample.in')
     subject = execute('./addition-and-subtraction < sample.in')
     trivial = execute('python3 addition-and-subtraction.py < sample.in')
 
-    subject_out = get_output(subject)
-    trivial_out = get_output(trivial)
-
-    if subject_out == trivial_out:
+    if subject == trivial:
         return 0
     else:
         print('############### ERROR on test: ', TESTNBR, ' ###############')
-        print('---- INPUT: ', d)
+        print('---- INPUT: ', dictionary)
         print("---- Subject OUTPUT: ")
-        print(subject_out)
+        print(subject)
         print("---- Correct OUTPUT: ")
-        print(trivial_out)
+        print(trivial)
 
     return 1
 
 
 class LurrencyTest(TestCase):
-    def testMultiplication(self):
+    def testCornerCases(self):
         # Corner tests
-        testUnit(1, 1, 1)
-        testUnit(1, 1000, 1)
-        testUnit(1, 1, 1000)
-        testUnit(1000, 1000, 1)
-        testUnit(1000, 1000, 1000)
+        self.assertEqual(testUnit(1, 1, 1), 0)
+        self.assertEqual(testUnit(1, 1000, 1), 0)
+        self.assertEqual(testUnit(1, 1, 1000), 0)
+        self.assertEqual(testUnit(1000, 1000, 1), 0)
+        self.assertEqual(testUnit(1000, 1000, 1000), 0)
         # self.assertEqual(testUnit(1, 1, 1), 0)
 
     # Random tests
-    # until[$X - gt 1000]
-    # do
-    # Y = 1
-    # Z = 1
-    # # Loop Y
-    # until[$Y - gt 1000]
-    # do
-    # # Loop X
-    # Z = 1
-    #  until[$Z - gt 1000]
-    #   do
-    #    # Loop Z
-    #    echo "Test [$i] X=$X Y=$Y Z=$Z \t\t - OK"
-    #      echo "$X $Y $Z \n" > sample.in
-
-    #       # solution
-    #       ./addition-and-subtraction < sample.in > out
-    #        # trivial
-    #        python3 addition-and-subtraction.py < sample.in > out_trivial
-    #         # Check ans
-    #         diff out out_trivial > /dev/null
-
-    #         if [$? -ne '0']
-    #          then
-    #           echo "Error on test"
-    #            return 13
-    #             fi
-
-    #             Z =$((Z+65))
-    #             i =$((i+1))
-    #         done
-    #         Y =$((Y+65))
-    #     done
-
-    #     X =$((X+65))
-    # done
-    # echo " -- ALL $i TEST OK -- "
+    def testRandomCases(self):
+        n, lower, upper = 2, 2, 1000
+        for i in range(lower, upper, int((upper - lower) / 100)):
+            x, y, z = random.randint(lower, i), random.randint(
+                lower, i), random.randint(lower, i)
+            self.assertEqual(testUnit(x, y, z), 0)
 
 
 if __name__ == '__main__':
-    x = LurrencyTest()
-    x.testMultiplication()
-    # unittest.main()
+    # test = LurrencyTest()
+    # test.cornerCases()
+    # test.randomCases()
+
+    unittest.main()
